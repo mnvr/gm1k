d732c83bb294be3c6a4e418fdfa645fb55019d0a
Manav <manav@mrmr.io>
2024-01-14 17:22:52 +0530

    Bytebeat!
    This is the first formula on viznut's (the person who kickstarted the whole
    phenomena) list.
    
    http://viznut.fi/demos/unix/bytebeat_formulas.txt

--- docs/generator.js
+++ docs/generator.js
@@ -1,5 +1,8 @@
 // Our custom generator needs to be defined in a separate file.
 class Generator extends AudioWorkletProcessor {
+    // Keep track of time
+    t = 0;
+
     // It will be called with each 128 frames (per channel).
     process(_, outputs) {
         // It'll have one output (and since this is a generator, no inputs).
@@ -9,11 +12,16 @@ class Generator extends AudioWorkletProcessor {
         // - Each channel is a Float32Array containing 128 samples.
         // - Each sample is in the range [-1, 1].
 
+        let t = this.t;
         for (const ch of channels) {
+            t = this.t;
             for (let i = 0; i < ch.length; i++) {
-                ch[i] = (2 * i) / ch.length - 1;
+                // From http://viznut.fi/demos/unix/bytebeat_formulas.txt
+                ch[i] = t * (((t >> 12) | (t >> 8)) & (63 & (t >> 4)));
+                t++;
             }
         }
+        this.t = t;
 
         // Return true to let the AudioContext know that we're still generating.
         return true;
