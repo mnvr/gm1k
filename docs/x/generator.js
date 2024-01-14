a63d13b0017b21fc3837407a138240478e9c8fc8
Manav <manav@mrmr.io>
2024-01-14 20:42:41 +0530

    Make it safer and add some variations
    Use a nicer sounding variation under our safety distortions

--- docs/x/generator.js
+++ docs/x/generator.js
@@ -16,19 +16,25 @@ class Generator extends AudioWorkletProcessor {
         for (const ch of channels) {
             t = this.t;
             for (let i = 0; i < ch.length; i++) {
-                const z = (this.gen(t) % 256 / 256);
-                ch[i] = (2 * (i / ch.length) - 1) * 0.01;
+                // Convert the output of gen into a number between 0 - 1
+                //
+                // This decimates the original sound a bit, but for now it is
+                // more important to not accidentally produce very loud noises.
+                const z = (this.gen3(t) % 256) / 256;
+                // Then map it to [-1, 1]. But that's too loud, esp on Safari.
+                // So multiply it by 0.02. Maybe too low, but we don't want to
+                // blow up in someone's earphones.
+                ch[i] = (2 * z - 1) * 0.01;
                 t++;
             }
         }
-        // t = t % 65536;
         this.t = t;
 
         // Return true to let the AudioContext know that we're still generating.
         return true;
     }
 
-    // Bytebeat generator
+    // The original Bytebeat
     //
     // Input is the time t, a monotonically increasing integral value.
     // Outputs should be a value betwen 0 and 256.
@@ -39,6 +45,41 @@ class Generator extends AudioWorkletProcessor {
         // http://countercomplex.blogspot.com/2011/10/algorithmic-symphonies-from-one-line-of.html
         // Kiitos!
         return t * (((t >> 12) | (t >> 8)) & (63 & (t >> 4)));
+    }
+
+    // 42 melody
+    gen2(t) {
+        // From https://dollchan.net/bytebeat
+        //
+        // The 42 melody, with decay (by Zackx)
+        //
+        // The original 42 melody is t*(42&t>>10)
+        return (((t * (42 & (t >> 10))) % 256) * (-t & 2047)) / 2e3;
+    }
+
+    // Sierpinski Harmony
+    gen3(t) {
+        // From https://dollchan.net/bytebeat
+        //
+        // Long Sierpinski Harmony (by viznut)
+        return ((5 * t) & (t >> 7)) | ((3 * t) & (t >> 10));
+    }
+
+    // Lost in Space
+    gen4(t) {
+        // From https://dollchan.net/bytebeat
+        //
+        // Lost in Space (by xpansive)
+        return (
+            ((t * ((t >> 8) | (t >> 9))) & 46 & (t >> 8)) ^
+            ((t & (t >> 13)) | (t >> 6))
+        );
+    }
+
+    // Untitled by tejeez
+    gen5(t) {
+        // From https://dollchan.net/bytebeat
+        return (t * ((t >> 5) | (t >> 8))) >> (t >> 16);
     }
 }
 
